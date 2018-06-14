f9feae6324e070c98a02f780d4fa13bbdd1154db
jatinkashyap <jatinkashyap9@gmail.com>
2018-06-13 22:56:41 -0400

    Jun13commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hii I am React </h1>
+        <Person name="jatin" age="29"/>
+        <Person name="ginni" age="27"> Good girl </Person>
+        <Person name="avi" age="2"/>
       </div>
     );
   }
